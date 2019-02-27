498cda3c2511e3401ebb405ff4b639aa0cf067bf
SilviuTroscot <silviu.troscot10@gmail.com>
2019-02-27 23:38:46 +0000

    implemented scaled fft

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,25 @@
 import numpy as np
+from scipy.io import wavfile
 
 def scaled_fft_db(x):
-    """ ASSIGNMENT 1:
-        a) Compute a 512-point Hann window and use it to weigh the input data.
-        b) Compute the DFT of the weighed input, take the magnitude in dBs and
-        normalize so that the maximum value is 96dB.
-        c) Return the first 257 values of the normalized spectrum
+    # generate 512 point Hann window
+    N = len(x)    # number of samples
+    n = np.arange(N)
 
-        Arguments:
-        x: 512-point input buffer.
+    # Compute the constant c for the Hann window
+    w_cos = np.cos((2 * np.pi * n)/(N - 1))
+    sum_sq = np.sum((w_cos/2)**2)
+    c = np.sqrt((N-1)/sum_sq)
 
-        Returns:
-        first 257 points of the normalized spectrum, in dBs
-    """
+    # Apply the Hanning window
+    x = x * c/2 * (1 - np.cos(2 * np.pi * n/(N - 1)))
+    # compute the rfft of the input, weighted by the 512 points Hann window
+    weighted_input_fft = np.abs(np.fft.rfft(x))
 
-    # Your code goes here
-
+    # convert the amplitude to dBs for the non-zero samples
+    non_zero_indices = np.where(weighted_input_fft != 0)[0]
+    weighted_input_fft_in_db = 20*np.log10(weighted_input_fft[non_zero_indices])
+    # normalize to have the maximum magnitude=96 dB
+    weighted_input_fft_in_db = 96-max(weighted_input_fft_in_db) + weighted_input_fft_in_db
+    
+    return weighted_input_fft_in_db
